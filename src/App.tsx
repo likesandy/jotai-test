592c9fa18ecf9c21407d98c3fa199077968d4281
tao <1445709137@qq.com>
2024-05-17 17:45:32 +0800

    📃 docs(添加注释):

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import { atom, useAtom } from 'jotai'
+import { atom, useAtom, useStore } from 'jotai'
 
 const aaaAtom = atom(0)
 const bbbAtom = atom(0)
+// 派生原子；基于其他原子定义状态
 // const sumAtom = atom((get) => get(aaaAtom) + get(bbbAtom))
 
 function App() {
+  // 获取atom状态
+  // 虽然store.get(xx)也可以获取
+  // 但是这个hook会自动订阅，当状态变化时组件会重新渲染
   // const sumCount = useAtom(sumAtom)
+
+  // useAtom = [useAtomValue,useSetAtom]
+  useStore().sub(aaaAtom, () => {
+    console.log('aaaAtom变化了')
+  })
   return (
     <>
       <Aaa />
@@ -37,4 +46,3 @@ function Bbb() {
 }
 
 export default App
-
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
+import { Provider } from 'jotai'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  // 默认会创建一个store，无需传store
+  <Provider>
+    <App />
+  </Provider>
+)
 
